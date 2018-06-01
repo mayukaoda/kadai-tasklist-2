a7675dff693e78d69be3ae69b483c9ef6a9e0700
EC2 Default User <ec2-user@ip-172-31-19-159.us-east-2.compute.internal>
2018-06-01 03:10:46 +0000

    bootstrap design

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -17,13 +17,12 @@
                 @foreach ($tasks as $task)
                     <tr>
                         <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                        <td>{{ $task->title }}</td>
+                        <td>{{ $task->status }}</td>
                         <td>{{ $task->content }}</td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
     @endif
-    {!! link_to_route('tasks.create', '新規タスクの投稿', null, ['class' => 'btn btn-primary']) !!}
-    
+    <a class="btn btn-primary btn-sm" href="{{ route('tasks.create') }}"><span stytasks.createle="color:#11569E" class='glyphicon glyphicon-plus'></span>  新規タスクの投稿</a>
 @endsection
