3096c62a32cbed1ec226c682d0728af84eb84884
EC2 Default User <ec2-user@ip-172-31-19-159.us-east-2.compute.internal>
2018-06-05 06:32:11 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', 'Taskcontroller@index');
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+Route::resource('tasks', 'Taskcontroller');
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', 'Taskcontroller@index');
-
-Route::resource('tasks', 'Taskcontroller');
