be3ec5a3132c8ca5b482b0ff6929412a7ef9b549
EC2 Default User <ec2-user@ip-172-31-19-159.us-east-2.compute.internal>
2018-06-05 05:12:10 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/', 'Taskcontroller@index');
 
 Route::resource('tasks', 'Taskcontroller');
